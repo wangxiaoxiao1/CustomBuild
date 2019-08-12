ad4d808d5ce3f5e55a2581a88c34b949089bf8c0
骁骁 王 <852616797@qq.com>
2019-08-12 19:33:14 +0800

    aaa

---
+++ CB/CB.Server/Controllers/DBSettingsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CB.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CB.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DBSettingsController : ControllerBase
+    {
+        public DBSettingsController()
+        {
+
+        }
+        [HttpGet]
+        public IEnumerable<DBLink> Get()
+        {
+            return new List<DBLink>() { new DBLink {  IP = "10.50.132.10", UserName = "sa", PWD = "1234.com" ,Name ="acs",DBName="fastdev"},
+             new DBLink {  IP = "10.50.132.10", UserName = "sa", PWD = "1234.com" ,Name ="acsorder",DBName="acsorder"}}.ToArray();
+        }
+    }
+}
---
+++ CB/CB.Shared/DBLink.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CB.Shared
+{
+  
+
+    public class DBLink
+    {
+        public string Name { get; set; }
+        public string DBName { get; set; }
+        public string IP { get; set; }
+        public string UserName { get; set; }
+        public string PWD { get; set; }
+
+    }
+}
