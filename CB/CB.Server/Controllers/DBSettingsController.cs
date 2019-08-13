2da70aee0a814d71025f5e3a5b65181250ee9235
骁骁 王 <852616797@qq.com>
2019-08-13 20:31:05 +0800

    a

--- CB/CB.Server/Controllers/DBSettingsController.cs
+++ CB/CB.Server/Controllers/DBSettingsController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using CB.Shared;
@@ -17,10 +19,48 @@ namespace CB.Server.Controllers
 
         }
         [HttpGet]
-        public IEnumerable<DBLink> Get()
+        public List<CBTable> Get()
         {
-            return new List<DBLink>() { new DBLink {  IP = "10.50.132.10", UserName = "sa", PWD = "1234.com" ,Name ="acs",DBName="fastdev"},
-             new DBLink {  IP = "10.50.132.10", UserName = "sa", PWD = "1234.com" ,Name ="acsorder",DBName="acsorder"}}.ToArray();
+            //return new List<DBLink>() { new DBLink {  IP = "10.50.132.10", UserName = "sa", PWD = "1234.com" ,Name ="acs",DBName="fastdev"},
+            // new DBLink {  IP = "10.50.132.10", UserName = "sa", PWD = "1234.com" ,Name ="acsorder",DBName="acsorder"}}.ToArray();
+            List<CBTable> cBTables = new List<CBTable>();
+            SqlConnection conn = new SqlConnection("server=devsqldb;database=fastdev;uid=sa;pwd=1234.com;ApplicationIntent=ReadOnly;");
+            conn.Open();
+            using (var da = new SqlDataAdapter())
+            {
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM dbo.BSMODULE SELECT * FROM dbo.BSMODULE";
+                da.SelectCommand =cmd;
+                DataSet ds = new DataSet();
+
+                // Fill the DataSet using default values for DataTable names, etc
+                da.Fill(ds);
+
+                    conn.Close();
+                // Return the dataset
+
+                foreach(DataTable tb in ds.Tables)
+                {
+                    CBTable table = new CBTable() { TableName = tb.TableName };
+                    foreach(DataColumn col in tb.Columns)
+                    {
+                        table.Columns.Add(new CBDataColumn { Name = col.ColumnName, Type = col.DataType.Name });
+                    }
+                    foreach (DataRow row in tb.Rows)
+                    {
+                        CBDataRow dataRow = new CBDataRow();
+                        for(int i=0;i< row.ItemArray.Length;i++)
+                        {
+
+                            dataRow.Cells.Add(new Cell { Column = table.Columns[i], Value = row.ItemArray[i] });
+                        }
+                        table.DataRows.Add(dataRow);
+                    }
+                    cBTables.Add(table);
+                }
+                return cBTables;
+            }
         }
     }
 }
---
+++ CB/CB.Shared/DBResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CB.Shared
+{
+    public class CBTable
+    {
+        public CBTable()
+        {
+            Columns = new List<CBDataColumn>();
+            DataRows = new List<CBDataRow>();
+        }
+        public string TableName { get; set; }
+        public List<CBDataColumn> Columns { get; set; }
+
+        public List<CBDataRow> DataRows { get; set; }
+    }
+    public class CBDataRow
+    {
+        public CBDataRow()
+        {
+            Cells = new List<Cell>();
+        }
+        public List<Cell> Cells { get; set; }
+    }
+    public class Cell
+    {
+        public object Value { get; set; }
+        public CBDataColumn Column { get; set; }
+    }
+    public class CBDataColumn
+    {
+        public string Name { get; set; }
+        public string    Type { get; set; }
+    }
+}
